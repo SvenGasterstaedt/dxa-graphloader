d3131c592c8092527efe7ddb62dbd98169b3d0a7
Svgas100 <svgas100@hhu.de>
2019-01-31 21:38:33 +0100

    Application runnable on dxram

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ./gradlew build
+cp /home/sven/Schreibtisch/git/dxa-graphloader/build/libs/dxa-graphloader.jar /home/sven/dxram/dxapp
--- src/main/java/de/hhu/bsinfo/dxapp/GraphLoader.java
+++ src/main/java/de/hhu/bsinfo/dxapp/GraphLoader.java
@@ -1,19 +1,34 @@
 package de.hhu.bsinfo.dxapp;
 
-import java.util.Arrays;
-
 import de.hhu.bsinfo.dxram.app.AbstractApplication;
 import de.hhu.bsinfo.dxram.boot.BootService;
 import de.hhu.bsinfo.dxram.engine.DXRAMVersion;
 import de.hhu.bsinfo.dxram.generated.BuildConfig;
-import de.hhu.bsinfo.dxutils.NodeID;
+import de.hhu.bsinfo.dxram.net.NetworkService;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 /**
- * "Hello world" example DXRAM application
+ * "GraphLoader"
  *
  * @author Stefan Nothaas, stefan.nothaas@hhu.de, 17.05.17
  */
 public class GraphLoader extends AbstractApplication {
+
+    private static final Logger LOGGER = LogManager.getFormatterLogger(AbstractApplication.class.getSimpleName());
+    private List<Short> peers;
+    private int peer_count;
+    private RandomAccessFile file;
+
+
     @Override
     public DXRAMVersion getBuiltAgainstVersion() {
         return BuildConfig.DXRAM_VERSION;
@@ -21,18 +36,62 @@ public class GraphLoader extends AbstractApplication {
 
     @Override
     public String getApplicationName() {
-        return "HelloWorld";
+        return "GraphLoader";
     }
 
     @Override
     public void main(final String[] p_args) {
         BootService bootService = getService(BootService.class);
+        NetworkService networkService = getService(NetworkService.class);
 
-        System.out.println("Hello, I am application " + getApplicationName() + " on a peer and my node id is " +
-                NodeID.toHexString(bootService.getNodeID()) + " with " + p_args.length + " cmd args: " +
-                Arrays.toString(p_args));
+        //Parameter überprüfung
+        if (p_args.length < 2) {
+            LOGGER.error("Parameters required! <filepath> <format> [optional:peer_count]");
+            LOGGER.error("Terminated!");
+            return;
+        }
+        String file_path = p_args[0];
+        String format = p_args[1];
+        try {
+            if (Files.isRegularFile(Paths.get(file_path))) {
+                LOGGER.info("Found file: " + file_path);
+                file = new RandomAccessFile(new File(file_path), "r");
+            } else {
+                LOGGER.error("File not found: " + file_path);
+                LOGGER.error("Terminated!");
+                return;
+            }
 
-        // Put your application code running on the DXRAM node/peer here
+            peers = bootService.getOnlinePeerNodeIDs();
+            peer_count = peers.size();
+
+            long aprox_chunk_size = file.length() / peer_count;
+            //networkService.sendMessage();
+            LOGGER.debug("Chunk Size: " + (aprox_chunk_size/(1024)) + "KB");
+
+            for (short p : peers) {
+                System.out.println(bootService.getNodeRole(p).toString());
+            }
+            System.out.println("Total peers: " + peer_count);
+
+            JSONParser jsonParser = new JSONParser("formats.json");
+
+        } catch (IOException ioex) {
+            LOGGER.trace(ioex.getMessage());
+            LOGGER.error("Terminated!");
+            return;
+        }
+
+        try {
+            Class exampleClass = Class.forName(format);
+            Object ob = exampleClass.newInstance();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -40,5 +99,6 @@ public class GraphLoader extends AbstractApplication {
         // Interrupt any flow of your application and make sure it shuts down.
         // Do not block here or wait for something to shut down. Shutting down of your application
         // must be execute asynchronously
+        System.out.println("Got killed!");
     }
 }
