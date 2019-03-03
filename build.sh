22c9d4088f4270ce2ef041bc4c2689ebdf2a12e8
Svgas100 <svgas100@hhu.de>
2019-03-03 16:32:36 +0100

    Writing

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./gradlew build
-cp /home/sven/Schreibtisch/git/dxa-graphloader/build/libs/dxa-graphloader.jar /home/sven/dxram/dxapp
+cp /home/sven/Schreibtisch/git/dxa-graphloader/build/libs/dxa-graphloader.jar /home/sven/dxram/plugin
